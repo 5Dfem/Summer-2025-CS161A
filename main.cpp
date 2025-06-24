3485c361871eb13a317a689515a873f0b57eeec1
5Dfem <5dimensionalfem@gmail.com>
2025-06-24 19:23:52 +0000

    created test program in main

--- main.cpp
+++ main.cpp
@@ -39,8 +39,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
+
+  string name;
+  cout << "Please enter your name: ";
+  cin >> name;
+  cout << "Hello " + name << endl;
   return 0;
+  
 }
 
 
@@ -48,9 +53,9 @@ int main() {
 
 // ------------- DESIGN -------------
 /* 
-Program Name:
+Program Name: 
 
-Program Description:
+Program Description: 
 
 Design:
 A. INPUT
